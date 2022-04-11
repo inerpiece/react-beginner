8f369aea8b0f1ded62c858d7452d0b2fc3d1935e
inerpiece <anton.p.markov@gmail.com>
2022-04-11 14:47:31 +0200

    Added components, styled them and utilized the 'props' functionality.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import Todo from './components/Todo'
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Hi there!'/>
+      <Todo text='How are you?!'/>
+      <Todo text='Perfect!'/>
+    </div>
   );
 }
 
